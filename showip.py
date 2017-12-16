593fab95fe1752fbbedc86abcedfe8517e425499
leozdgao <gzdleo0816@outlook.com>
2017-12-17 00:59:37 +0800

    init

---
+++ showip.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+import sys
+import socket
+
+# Workflow3 supports Alfred 3's new features. The `Workflow` class
+# is also compatible with Alfred 2.
+from workflow import Workflow3
+
+
+def main(wf):
+    # The Workflow3 instance will be passed to the function
+    # you call from `Workflow3.run`.
+    # Not super useful, as the `wf` object created in
+    # the `if __name__ ...` clause below is global...
+    #
+    # Your imports go here if you want to catch import errors, which
+    # is not a bad idea, or if the modules/packages are in a directory
+    # added via `Workflow3(libraries=...)`
+    # import somemodule
+    # import anothermodule
+
+    # Get args from Workflow3, already in normalized Unicode.
+    # This is also necessary for "magic" arguments to work.
+    # args = wf.args
+
+    # Do stuff here ...
+    localhost = socket.gethostname()
+    localip = socket.gethostbyname(localhost)
+
+    # Add an item to Alfred feedback
+    wf.add_item(u'本地 IP 地址', localip, valid=True, arg=localip)
+
+    # Send output to Alfred. You can only call this once.
+    # Well, you *can* call it multiple times, but subsequent calls
+    # are ignored (otherwise the JSON sent to Alfred would be invalid).
+    wf.send_feedback()
+
+
+if __name__ == '__main__':
+    # Create a global `Workflow3` object
+    wf = Workflow3()
+    # Call your entry function via `Workflow3.run()` to enable its
+    # helper functions, like exception catching, ARGV normalization,
+    # magic arguments etc.
+    sys.exit(wf.run(main))
